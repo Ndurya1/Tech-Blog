dfc1adac0bbf6686463804581873f9227d804590
Ndurya <nduryamuhammad6@gmail.com>
2025-11-27 12:26:09 +0300

    created superuser

--- backend/Blog/models.py
+++ backend/Blog/models.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 class Blog(models.Model):
     image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
     title = models.CharField(max_length=200)
-    summary = models.CharField(max_length=300)
+    summary = models.CharField(max_length=1000)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
